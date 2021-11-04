1a3ba621b2a91b411a3c6c20ce8673efd69a95a7
Balika4222 <72438034+rozmanbalazs@users.noreply.github.com>
2021-11-04 14:23:23 +0100

    Add files via upload

---
+++ egyenletek.py
@@ -0,0 +1,26 @@
+def elso():
+    print("teszt")
+
+def masod():
+    print("teszt1")
+
+while True:
+    try:
+        print("<--- Mód választás --->")
+        mode_select = int(input("Elsőfokú - 0, Másodfokú - 1\n input: "))
+
+        if mode_select == 0 or mode_select == 1:
+            break
+
+        else:
+            raise ValueError
+
+    except ValueError:
+        print("\nhibás Érték (csak 0, 1 et használj)")
+
+
+if mode_select == 0:
+    elso()
+
+elif mode_select == 1:
+    masod()
