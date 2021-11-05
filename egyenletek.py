7fda42110bd62b0fe5a0d4331275adacf91e0b43
Balika4222 <72438034+rozmanbalazs@users.noreply.github.com>
2021-11-05 09:32:16 +0100

    Add files via upload

--- egyenletek.py
+++ egyenletek.py
@@ -1,6 +1,25 @@
-def elso():
-    print("teszt")
+import sympy as sym
 
+def elso():
+    print("---Írd be az első fokú egyenletedet---")
+    print("Egyenlet beviteli példa: 2*x+6-5,6")
+    print("A példa értéke: 2x+6-5=6")
+
+    while True:
+        try:
+            raw_egyenlet_input = input("Egyenlet: ")
+            
+            
+            symbol = sym.symbols('x y a b c')
+            eq1 = sym.Eq( raw_egyenlet_input)
+
+            result = sym.solve(eq1,symbol)
+            result = str(result)[1:-1]
+            print(f"a megoldás X = {result}")
+
+        except ValueError:
+            print("Hibás egyenlet")
+    
 def masod():
     print("teszt1")
 
