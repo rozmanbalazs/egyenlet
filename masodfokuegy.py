b1feeb99daa1557dc330e115fc93987eeadec5a2
Szőnyi Bence <70972591+Benceboy100@users.noreply.github.com>
2021-11-10 08:54:36 +0100

    Update masodfokuegy.py

--- masodfokuegy.py
+++ masodfokuegy.py
@@ -1,16 +1,1 @@
-#BALÁZS EZT MÉG NE HASZNÁLD MERT MŰKÖDIK DE ELVILEG SZAR
-#NEM TUDOM ÓRÁN TESZTELNI, ÍGY JÓ LEHET
 
-
-
-from sympy import symbols, solve
-import math
-a=float(input("Szám1:"))
-b=float(input("Szám2:"))
-x=symbols('x')
-expr1 = (x**2)+a+b
-expr = math.sqrt(expr)
-sol = solve(expr)
-
-sol
-print(sol)
