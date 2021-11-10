95bedc7e7ad6d3b2cedc65efec818dab5f9b049c
Szőnyi Bence <70972591+Benceboy100@users.noreply.github.com>
2021-11-10 09:01:29 +0100

    Update masodfokuegy.py

--- masodfokuegy.py
+++ masodfokuegy.py
@@ -1,1 +1,9 @@
+import math
 
+a=float(input("Hány x**2 van?")
+b=float(input("B szám az egyenletben: ?")
+c=float(input("C szám az egyenletben: ?")
+solver = (b*-1 + math.sqrt(4*a*c))/2*a
+solverNEG = (b*-1 - math.sqrt(4*a*c))/2*a
+        
+print(solver, solverNEG)
